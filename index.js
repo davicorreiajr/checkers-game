e563b7d45510376ba3052e1b075c15b9d598f46f
Davi Correia <davicorreiajr@gmail.com>
2017-11-10 20:35:35 -0200

    Added process option to port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((request, response) => {
   console.log('req.url = ', request.url);
