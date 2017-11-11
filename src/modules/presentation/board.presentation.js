768110e51a66fead0e7609e734ac63122bcb3813
Davi Correia <davicorreiajr@gmail.com>
2017-11-11 18:36:27 -0200

    Imported use cases from board.preentation

--- src/modules/presentation/board.presentation.js
+++ src/modules/presentation/board.presentation.js
@@ -1,10 +1,26 @@
+import { GetLightPiecesLocationUseCase } from '../domain/get-light-pieces-location.use-case';
+import { GetDarkPiecesLocationUseCase } from '../domain/get-dark-pieces-location.use-case';
+
 export const BoardPresentation = (() => {
   function test(str) {
     console.log(str + 'module');
   }
 
+  const onStart = () => {
+    setPiecesLocation();
+  }
+
+  const setPiecesLocation = () => {
+    const lightPiecesLocation = GetLightPiecesLocationUseCase.execute();
+    const darkPiecesLocation = GetDarkPiecesLocationUseCase.execute();
+
+    console.log('lightPiecesLocation', lightPiecesLocation);
+    console.log('darkPiecesLocation', darkPiecesLocation);
+  }
+
   return {
-    test
+    onStart,
+    setPiecesLocation
   };
 })()
 
