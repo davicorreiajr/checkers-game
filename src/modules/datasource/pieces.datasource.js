01b47af9071785f27f35015fab6c7afac0fa2bcd
Davi Correia <davicorreiajr@gmail.com>
2017-11-11 18:28:13 -0200

    Created PiecesDataSource with simple gets methods

---
+++ src/modules/datasource/pieces.datasource.js
@@ -0,0 +1,44 @@
+export const PiecesDataSource = (() => {
+  const darkPiecesLocation = {
+    0: 'A1',
+    1: 'C1',
+    2: 'E1',
+    3: 'G1',
+    4: 'B2',
+    5: 'D2',
+    6: 'F2',
+    7: 'H2',
+    8: 'A3',
+    9: 'C3',
+    10: 'E3',
+    11: 'G3',
+  }
+
+  const lightPiecesLocation = {
+    0: 'A1',
+    1: 'C1',
+    2: 'E1',
+    3: 'G1',
+    4: 'B2',
+    5: 'D2',
+    6: 'F2',
+    7: 'H2',
+    8: 'A3',
+    9: 'C3',
+    10: 'E3',
+    11: 'G3',
+  }
+
+  const getDarkPiecesLocation = () => {
+    return darkPiecesLocation;
+  }
+
+  const getLightPiecesLocation = () => {
+    return lightPiecesLocation;
+  }
+
+  return {
+    getDarkPiecesLocation,
+    getLightPiecesLocation
+  }
+})()
