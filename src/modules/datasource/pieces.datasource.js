36283854f018953b61e9e2097d82fed6bbb2ba00
Davi Correia <davicorreiajr@gmail.com>
2017-11-11 20:14:29 -0200

    Fixed possible places to the pieces

--- src/modules/datasource/pieces.datasource.js
+++ src/modules/datasource/pieces.datasource.js
@@ -37,18 +37,26 @@ export const PiecesDataSource = (() => {
     return lightPiecesLocation;
   }
 
-  const getDarkPiecesInitialLocation = () => {
-    return ['A1', 'C1', 'E1', 'G1', 'B2', 'D2', 'F2', 'H2', 'A3', 'C3', 'E3', 'G3'];
+  const getDarkPiecesPossibleLocation = () => {
+    return[
+      'A1', 'C1', 'E1', 'G1', 'B2', 'D2', 'F2', 'H2', 'A3', 'C3', 'E3', 'G3',
+      'B4', 'D4', 'F4', 'H4', 'A5', 'C5', 'E5', 'G5', 'B6', 'D6', 'F6', 'H6',
+      'A7', 'C7', 'E7', 'G7', 'B8', 'D8', 'F8', 'H8'
+    ];
   }
 
-  const getLightPiecesInitialLocation = () => {
-    return ['A8', 'C8', 'E8', 'G8', 'B7', 'D7', 'F7', 'H7', 'A6', 'C6', 'E6', 'G6'];
+  const getLightPiecesPossibleLocation = () => {
+    return [
+      'A8', 'C8', 'E8', 'G8', 'B7', 'D7', 'F7', 'H7', 'A6', 'C6', 'E6', 'G6',
+      'B5', 'D5', 'F5', 'H5', 'A4', 'C4', 'E4', 'G4', 'B3', 'D3', 'F3', 'H4',
+      'A2', 'C2', 'E2', 'G2', 'B1', 'D1', 'F1', 'H1'
+    ];
   }
 
   return {
     getDarkPiecesLocation,
     getLightPiecesLocation,
-    getDarkPiecesInitialLocation,
-    getLightPiecesInitialLocation
+    getDarkPiecesPossibleLocation,
+    getLightPiecesPossibleLocation
   };
 })()
