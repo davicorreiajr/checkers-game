1d237a0f2747ac99c6460d3037f82f2e0d2032ee
Davi Correia <davicorreiajr@gmail.com>
2017-11-11 18:38:02 -0200

    Fixed initial place of light pieces

--- src/modules/datasource/pieces.datasource.js
+++ src/modules/datasource/pieces.datasource.js
@@ -15,18 +15,18 @@ export const PiecesDataSource = (() => {
   }
 
   const lightPiecesLocation = {
-    0: 'A1',
-    1: 'C1',
-    2: 'E1',
-    3: 'G1',
-    4: 'B2',
-    5: 'D2',
-    6: 'F2',
-    7: 'H2',
-    8: 'A3',
-    9: 'C3',
-    10: 'E3',
-    11: 'G3',
+    0: 'B8',
+    1: 'D8',
+    2: 'F8',
+    3: 'H8',
+    4: 'A7',
+    5: 'C7',
+    6: 'E7',
+    7: 'G7',
+    8: 'B6',
+    9: 'D6',
+    10: 'F6',
+    11: 'H6',
   }
 
   const getDarkPiecesLocation = () => {
