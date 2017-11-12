cd8d45c9005ce1f3242cfe1b9a34e431d8da5a7d
Davi Correia <davicorreiajr@gmail.com>
2017-11-12 17:49:53 -0200

    Removed unused code

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,14 +1,3 @@
 import { BoardPresentation } from './presentation/board.presentation';
 
-console.log('BLAAA');
-
 BoardPresentation.onInit();
-
-window.onload = function() {
-  console.log('inside onload');
-  window.BoardPresentation = BoardPresentation;
-}
-
-document.onload = function() {
-  console.log('inside doc onload');
-}
