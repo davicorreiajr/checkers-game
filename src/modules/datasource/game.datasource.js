2cd432dd78058a300e3fa472d4867701d7fd5ca9
Davi Correia <davicorreiajr@gmail.com>
2017-11-12 11:43:09 -0200

    Created GameDataSource

---
+++ src/modules/datasource/game.datasource.js
@@ -0,0 +1,14 @@
+import { BehaviorSubject } from 'rxjs';
+import { PlayerVictorious } from '../shared/constants';
+
+export const GameDataSource = (() => {
+  let whoWon = new BehaviorSubject(PlayerVictorious.none);
+
+  const getWhoWon = () => {
+    return whoWon;
+  }
+
+  const setWhoWon = (decision) => {
+    whoWon.next(decision);
+  }
+})()
